389547a8754a3388ac90df24caf70d0221f18dc8
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-12 21:49:38 +0900

    [FEAT] Coordinator Pattern

---
+++ PPAK_CVS/Source/Home/HomeCoordinator.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class HomeCoordinator: Coordinator {
+
+  var navigationController: UINavigationController
+
+  init(navigationController: UINavigationController) {
+    self.navigationController = navigationController
+  }
+
+  func start() {
+    let viewController = HomeViewController()
+    self.navigationController.setViewControllers([viewController], animated: false)
+  }
+}
