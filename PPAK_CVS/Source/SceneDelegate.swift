a2e9aa83b042749326133ddcffc2528a9624527c
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-12 21:39:55 +0900

    [CHORE] new line

--- PPAK_CVS/Source/SceneDelegate.swift
+++ PPAK_CVS/Source/SceneDelegate.swift
@@ -11,7 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
     guard let scene = scene as? UIWindowScene else { return }
     let window = UIWindow(windowScene: scene)
     self.window = window
