f9a9ac2d6eb8d49ecdf30febad9c7b8a2d32da4f
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-12 21:34:25 +0900

    [REFACTOR] Swiftlint 적용

--- PPAK_CVS/ViewController.swift
+++ PPAK_CVS/ViewController.swift
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
-
