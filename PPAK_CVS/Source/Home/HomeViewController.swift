bdf940fe1dac94cc12359067cf3288960938a814
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-09-16 14:50:27 +0900

    [CHORE] HomeVC inheritance

--- PPAK_CVS/Source/Home/HomeViewController.swift
+++ PPAK_CVS/Source/Home/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class HomeViewController: UIViewController {
+final class HomeViewController: BaseViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
