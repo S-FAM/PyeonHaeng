e75e8d2bbfd0194ec7e406e0db9da19a14c5cff2
Minji Kim <minji_0801@naver.com>
2022-09-24 12:24:40 +0900

    [ADD] OnboardingVC

---
+++ PPAK_CVS/Sources/Scenes/Onboarding/OnboardingVC.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewController.swift
+//  PPAK_CVS
+//
+//  Created by 김민지 on 2022/09/24.
+//
+
+final class OnboardingViewController: BaseViewController {
+
+  // MARK: - Life Cycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  // MARK: - Setup
+
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+
+  override func setupStyles() {
+    super.setupStyles()
+  }
+}
