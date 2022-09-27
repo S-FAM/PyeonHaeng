2faeb8e986c7288d44d78651025c14abf5df7836
eung7 <wannabeeung@gmail.com>
2022-09-27 16:20:53 +0900

    [Feat] HomeViewModel 목업 구현 // 논의 해봐야 함 ...

---
+++ PPAK_CVS/Sources/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewModel.swift
+//  PPAK_CVS
+//
+//  Created by 김응철 on 2022/09/27.
+//
+
+import RxSwift
+import RxCocoa
+
+final class HomeViewModel {
+  struct Input {
+    
+  }
+  
+  struct Output {
+    
+  }
+  
+  let input = Input()
+  let output = Output()
+  let disposeBag = DisposeBag()
+  
+  init() {
+    bind()
+  }
+  
+  func bind() {
+    
+  }
+}
