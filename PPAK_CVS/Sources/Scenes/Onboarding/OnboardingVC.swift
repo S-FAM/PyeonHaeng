e408abf7be73d9af9b8685dcbdaa7c3f4798ce0f
Minji Kim <minji_0801@naver.com>
2022-10-01 17:05:38 +0900

    [CHORE] change OnboardingVC bgColor (need to change)

--- PPAK_CVS/Sources/Scenes/Onboarding/OnboardingVC.swift
+++ PPAK_CVS/Sources/Scenes/Onboarding/OnboardingVC.swift
@@ -220,8 +220,8 @@ extension OnboardingViewController {
   private enum Color {
     static let viewBgColor = UIColor.white
     static let pageIndicator = UIColor.lightGray
-    static let currentPageIndicator = UIColor.black // TODO: Color Model 정리되면 사용하기
-    static let bottomCurveViewBgColor = UIColor.black // TODO: Color Model 정리되면 사용하기
+    static let currentPageIndicator = UIColor.colorFromHex(hex: "#030026") // TODO: Color Model 적용
+    static let bottomCurveViewBgColor = UIColor.colorFromHex(hex: "#030026") // TODO: Color Model 적용
     static let titleLabel = UIColor.white
     static let descLabel = UIColor.white.withAlphaComponent(0.5)
   }
