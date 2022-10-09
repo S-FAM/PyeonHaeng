677ab6a5129e8eb130cb8f2a9a1b0901621cd837
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-09 22:44:38 +0900

    [ADD] NavigationBar Configuration
    - 우측 상단 하트 이미지 버튼
    - 가운데 로고를 넣을 ImageView

--- PPAK_CVS/Sources/Scenes/Product/ProductVC.swift
+++ PPAK_CVS/Sources/Scenes/Product/ProductVC.swift
@@ -15,4 +15,20 @@ final class ProductViewController: BaseViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+
+  override func setupLayouts() {
+    super.setupLayouts()
+
+    // navigation bar
+    navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(
+      image: UIImage(systemName: "heart"),
+      style: .plain,
+      target: self,
+      action: nil
+    )
+
+    navigationItem.titleView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 50)).then {
+      $0.backgroundColor = .systemGray
+    }
+  }
 }
