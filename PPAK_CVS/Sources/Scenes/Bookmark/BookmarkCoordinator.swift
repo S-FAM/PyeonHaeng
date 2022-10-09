34899a4eaf82586f70b2c305ce3768005699b252
eung7 <wannabeeung@gmail.com>
2022-10-09 12:03:04 +0900

    [Feat] BookmarkCoordinator 구현

--- PPAK_CVS/Sources/Scenes/Bookmark/BookmarkCoordinator.swift
+++ PPAK_CVS/Sources/Scenes/Bookmark/BookmarkCoordinator.swift
@@ -5,4 +5,18 @@
 //  Created by 김응철 on 2022/10/08.
 //
 
-import Foundation
+import UIKit
+
+final class BookmarkCoordinator: Coordinator {
+
+  var navigationController: UINavigationController
+
+  init(navigationController: UINavigationController) {
+    self.navigationController = navigationController
+  }
+
+  func start() {
+    let viewController = BookmarkViewController()
+    self.navigationController.setViewControllers([viewController], animated: true)
+  }
+}
