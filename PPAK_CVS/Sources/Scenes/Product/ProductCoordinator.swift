32b8eaf4d17c95deb1567248364ec713c3391d34
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-09 22:10:30 +0900

    [ADD] Coordinator, ViewController

---
+++ PPAK_CVS/Sources/Scenes/Product/ProductCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  ProductCoordinator.swift
+//  PPAK_CVS
+//
+//  Created by 홍승현 on 2022/10/09.
+//
+
+import Foundation
---
+++ PPAK_CVS/Sources/Scenes/Product/ProductVC.swift
@@ -0,0 +1,15 @@
+//
+//  ProductViewController.swift
+//  PPAK_CVS
+//
+//  Created by 홍승현 on 2022/10/09.
+//
+
+import UIKit
+
+final class ProductViewController: BaseViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
