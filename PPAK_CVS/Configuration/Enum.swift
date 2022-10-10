b91e002ae8a9510f6f0d6bc94a1cd3d6f8425055
isGeekCode <ptuguitar@naver.com>
2022-10-10 17:27:32 +0900

    [ADD] Enum 타입 eventType추가

--- PPAK_CVS/Configuration/Enum.swift
+++ PPAK_CVS/Configuration/Enum.swift
@@ -15,53 +15,76 @@ import UIKit
 ///     - fontColor: 태그의 폰트색상
 
 enum CVSType {
-    
-    case cu // cu
-    case gs // gs25
-    case sevenEleven // sevenEleven
-    case miniStop // miniStop
-    case eMart // eMart
-    
+
+  case cu // cu
+  case gs // gs25
+  case sevenEleven // sevenEleven
+  case miniStop // miniStop
+  case eMart // eMart
+
     var title: String {
-        switch self {
-        case .cu: return "CU"
-        case .gs: return "GS25"
-        case .sevenEleven: return "7-ELEVEn"
-        case .miniStop: return "MINISTOP"
-        case .eMart: return "emart24"
-        }
+      switch self {
+      case .cu: return "CU"
+      case .gs: return "GS25"
+      case .sevenEleven: return "7-ELEVEn"
+      case .miniStop: return "MINISTOP"
+      case .eMart: return "emart24"
+      }
+  }
+
+  // TODO: - 나중에 정확한 파일명으로 수정필요
+  var image: UIImage? {
+    switch self {
+      case .cu: return UIImage(systemName: "CU")
+      case .gs: return UIImage(systemName: "GS25")
+      case .sevenEleven: return UIImage(systemName: "7-ELEVEn")
+      case .miniStop: return UIImage(systemName: "MINISTOP")
+      case .eMart: return UIImage(systemName: "emart24")
     }
-    
-    // TODO: - 나중에 정확한 파일명으로 수정필요
-    var image: UIImage? {
-        switch self {
-        case .cu: return UIImage(systemName: "CU")
-        case .gs: return UIImage(systemName: "GS25")
-        case .sevenEleven: return UIImage(systemName: "7-ELEVEn")
-        case .miniStop: return UIImage(systemName: "MINISTOP")
-        case .eMart: return UIImage(systemName: "emart24")
-        }
-    }
-    
-    // TODO: - 머지후 정확한 컬러로 수정필요
-    var bgColor: UIColor {
-        switch self {
+  }
+
+  // TODO: - 머지후 정확한 컬러로 수정필요
+  var bgColor: UIColor {
+      switch self {
         case .cu: return UIColor.black
         case .gs: return UIColor.black
         case .sevenEleven: return UIColor.black
         case .miniStop: return UIColor.black
         case .eMart: return UIColor.black
-        }
-    }
-    
-    // TODO: - 머지후 정확한 컬러로 수정필요
-    var fontColor: UIColor {
-        switch self {
+      }
+  }
+
+  // TODO: - 머지후 정확한 컬러로 수정필요
+  var fontColor: UIColor {
+      switch self {
         case .cu: return UIColor.black
         case .gs: return UIColor.black
         case .sevenEleven: return UIColor.black
         case .miniStop: return UIColor.black
         case .eMart: return UIColor.black
-        }
-    }
+      }
+  }
 }
+
+
+/// Description
+/// 이벤트의 종류를 가리키는 Enum Type
+enum EventType {
+    
+    case onePlusOne
+    case twoPlusOne
+    case allEnText
+    case allKrText
+    
+    var eventTitle: String {
+        switch self {
+        case .onePlusOne: return "1 + 1"
+        case .twoPlusOne: return "2 + 1"
+        case .allEnText: return "All"
+        case .allKrText: return "전체"
+        }
+    }
+
+    
+}
+
