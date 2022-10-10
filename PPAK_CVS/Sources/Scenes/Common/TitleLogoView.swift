536ec395a72a76a48cd65be776273e0d8d6cb2c7
isGeekCode <ptuguitar@naver.com>
2022-10-10 20:28:22 +0900

    [CHORE] indent 수정 4>>2

--- PPAK_CVS/Sources/Scenes/Common/TitleLogoView.swift
+++ PPAK_CVS/Sources/Scenes/Common/TitleLogoView.swift
@@ -7,29 +7,29 @@
 
 import UIKit
 
- class TitleLogoView: UIView {
-
-     var titleLabel =  UILabel()
-     var cvsType: CVSType
-
-     init(cvsType: CVSType) {
-         self.cvsType = cvsType
-         super.init(frame: .zero)
-         setTitle()
-     }
-
-     required init?(coder: NSCoder) {
-         fatalError("init(coder:) has not been implemented")
-     }
-
-     func setTitle() {
-         addSubview(titleLabel)
-
-         titleLabel.snp.makeConstraints {
-             $0.centerX.centerY.equalToSuperview()
-         }
-         titleLabel.text = cvsType.title
-         titleLabel.textColor = cvsType.fontColor
-     }
-
-   }
+class TitleLogoView: UIView {
+  
+  var titleLabel =  UILabel()
+  var cvsType: CVSType
+  
+  init(cvsType: CVSType) {
+    self.cvsType = cvsType
+    super.init(frame: .zero)
+    setTitle()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setTitle() {
+    addSubview(titleLabel)
+    
+    titleLabel.snp.makeConstraints {
+      $0.centerX.centerY.equalToSuperview()
+    }
+    titleLabel.text = cvsType.title
+    titleLabel.textColor = cvsType.fontColor
+  }
+  
+}
