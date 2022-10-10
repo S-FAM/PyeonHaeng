b933b8ca100a17926872d764bcf3e2769c6120bc
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-10 19:09:17 +0900

    [FEAT] CollectionHeaderView
    figma 디자인가이드에 맞춰 UI 구현
    background 색상은 systemBackground로 설정(label과의 관계성 때문)

--- PPAK_CVS/Sources/Scenes/Product/ProductCollectionHeaderView.swift
+++ PPAK_CVS/Sources/Scenes/Product/ProductCollectionHeaderView.swift
@@ -10,6 +10,55 @@ import UIKit
 final class ProductCollectionHeaderView: UICollectionReusableView {
   static let id = "ProductCollectionHeaderView"
 
+  private let shareButton = UIButton().then {
+    var configuration = UIButton.Configuration.plain()
+    configuration.image = UIImage(systemName: "square.and.arrow.up")
+    $0.configuration = configuration
+  }
+
+  private let productImageView = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+    $0.backgroundColor = .systemBlue // temporary color
+  }
+
+  private let nameLabel = UILabel().then {
+    $0.font = Font.nameLabel
+    $0.text = "코카)씨그램피치탄산수350ML"
+  }
+
+  private let priceLabel = UILabel().then {
+    $0.font = Font.priceLabel
+    $0.text = "1,500원"
+  }
+
+  private let priceDiscriptionLabel = UILabel().then {
+    $0.font = Font.priceDescriptionLabel
+    $0.text = "(개당 750원)"
+  }
+
+  private let previousHistoryLabel = UILabel().then {
+    $0.font = Font.previousHistoryLabel
+    $0.text = "이전 행사 내역"
+    $0.textColor = .white
+  }
+
+  private let priceStackView = UIStackView().then {
+    $0.axis = .horizontal
+    $0.alignment = .center
+    $0.spacing = 3
+  }
+
+  private let wholeStackView = UIStackView().then {
+    $0.alignment = .center
+    $0.distribution = .fillProportionally
+    $0.axis = .vertical
+    $0.spacing = 10
+  }
+
+  private let curveView = BottomCurveView().then {
+    $0.backgroundColor = .systemPurple
+  }
+
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupLayouts()
@@ -27,14 +76,50 @@ final class ProductCollectionHeaderView: UICollectionReusableView {
 extension ProductCollectionHeaderView {
   private func setupLayouts() {
 
+    // == views ==
+    [shareButton, wholeStackView, curveView].forEach {
+      addSubview($0)
+    }
+
+    // == stackViews ==
+    [priceLabel, priceDiscriptionLabel].forEach {
+      priceStackView.addArrangedSubview($0)
+    }
+
+    [productImageView, nameLabel, priceStackView].forEach {
+      wholeStackView.addArrangedSubview($0)
+    }
+
+    // == curveView ==
+    curveView.addSubview(previousHistoryLabel)
   }
 
   private func setupConstraints() {
+    shareButton.snp.makeConstraints { make in
+      make.trailing.top.equalToSuperview().inset(Inset.shareButton)
+    }
 
+    wholeStackView.snp.makeConstraints { make in
+      make.centerX.equalToSuperview()
+      make.top.equalToSuperview().inset(Inset.StackView.top)
+    }
+
+    curveView.snp.makeConstraints { make in
+      make.leading.trailing.bottom.equalToSuperview()
+      make.top.equalTo(wholeStackView.snp.bottom).inset(Inset.curveView)
+    }
+
+    productImageView.snp.makeConstraints { make in
+      make.width.height.equalTo(240)
+    }
+
+    previousHistoryLabel.snp.makeConstraints { make in
+      make.bottom.trailing.equalToSuperview().inset(Inset.previousHistoryLabel)
+    }
   }
 
   private func setupStyles() {
-    
+    backgroundColor = .systemBackground
   }
 }
 
