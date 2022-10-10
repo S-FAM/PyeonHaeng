8c28c4eb56e333343c043e69ca486e743e7050fb
isGeekCode <ptuguitar@naver.com>
2022-10-10 17:11:30 +0900

    [ADD] LogoTagView 생성

---
+++ PPAK_CVS/Sources/Scenes/Common/LogoTagView.swift
@@ -0,0 +1,35 @@
+//
+//  LogoTagView.swift
+//  PPAK_CVS
+//
+//  Created by hyeonseok on 2022/10/03.
+//
+
+import UIKit
+
+ class LogoTagView: UIView {
+
+     var titleLabel =  UILabel()
+     var cvsType: CVSType
+
+     init(cvsType: CVSType) {
+         self.cvsType = cvsType
+         super.init(frame: .zero)
+         setTitle()
+     }
+
+     required init?(coder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+     }
+
+     func setTitle() {
+         addSubview(titleLabel)
+
+         titleLabel.snp.makeConstraints {
+             $0.centerX.centerY.equalToSuperview()
+         }
+         titleLabel.text = cvsType.title
+         titleLabel.textColor = cvsType.fontColor
+     }
+
+   }
