c2ec28f6133989e686629ff606dcdf7c66016b37
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-16 17:48:39 +0900

    [ADD] Coordinator properties & methods
    ## properties ##
    - childCoordinators
    - parentCoordinator
    
    ## methods ##
    
    - start(coordinator:)
    - finish(coordinator:)

--- PPAK_CVS/Sources/Coordinator.swift
+++ PPAK_CVS/Sources/Coordinator.swift
@@ -3,6 +3,10 @@ import UIKit
 protocol Coordinator {
 
   var navigationController: UINavigationController { get set }
+  var childCoordinators: [Coordinator] { get set }
+  var parentCoordinator: Coordinator? { get set }
 
   func start()
+  func start(coordinator: Coordinator)
+  func finish(coordinator: Coordinator)
 }
