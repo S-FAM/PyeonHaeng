59e4cc009d136897ce29211312246df910f4b5fe
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-29 17:20:03 +0900

    [FEAT] ProductHeaderViewViewModel
    - 공유 버튼 탭했을 떄 처리

--- PPAK_CVS/Sources/Scenes/Product/ProductHeaderViewViewModel.swift
+++ PPAK_CVS/Sources/Scenes/Product/ProductHeaderViewViewModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class ProductHeaderViewViewModel: ViewModel {
+
+  enum Action {
+    case share
+  }
+
+  enum Mutation {
+    case showShareWindow(Bool)
+  }
+
+  struct State {
+    var isShareButtonTapped: Bool = false
+  }
+
+  var initialState = State()
+}
+
+extension ProductHeaderViewViewModel {
+
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .share:
+      // prevent from multiple requests
+      guard self.currentState.isShareButtonTapped == false else { return .empty() }
+      return .concat([
+        .just(.showShareWindow(true)),
+        .just(.showShareWindow(false))
+      ])
+    }
+  }
+
+  func reduce(state: State, mutation: Mutation) -> State {
+    var nextState = state
+
+    switch mutation {
+    case let .showShareWindow(isShareButtonTapped):
+      nextState.isShareButtonTapped = isShareButtonTapped
+    }
+
+    return nextState
+  }
+}
