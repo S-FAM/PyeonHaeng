9f2e7844c9f31757bb80d0d4c1579fb453e2fc02
isGeekCode <ptuguitar@naver.com>
2022-11-03 21:26:18 +0900

    [CHORE] indent 수정

--- PPAK_CVS/Configuration/Extensions/UIColor+.swift
+++ PPAK_CVS/Configuration/Extensions/UIColor+.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIColor {
-  
+
   convenience init(hex: Int, alpha: CGFloat = 1.0) {
     let components = (
       red: CGFloat((hex >> 16) & 0xff) / 255,
@@ -21,7 +21,7 @@ extension UIColor {
 
 // MARK: - Store's SymbolColor
 extension UIColor {
-  
+
   static let cuBackGroundColor = UIColor(hex: 0x51485)
   static let cuFontColor = UIColor(hex: 0x9DC92A)
   static let gsBackGroundColor = UIColor(hex: 0x63514D)
