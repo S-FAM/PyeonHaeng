40e6df2413ffed47659df1d2e1d9d8896fcf9977
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-04 21:58:32 +0900

    [CHORE] title logo view
    access control 수정
    final 처리

--- PPAK_CVS/Sources/Scenes/Common/TitleLogoView.swift
+++ PPAK_CVS/Sources/Scenes/Common/TitleLogoView.swift
@@ -7,30 +7,30 @@
 
 import UIKit
 
-class TitleLogoView: UIView {
-  
-  var titleLabel =  UILabel()
-  var cvsType: CVSType
-  
+final class TitleLogoView: UIView {
+
+  private var titleLabel =  UILabel()
+  private var cvsType: CVSType
+
   init(cvsType: CVSType) {
     self.cvsType = cvsType
     super.init(frame: .zero)
     setTitle()
   }
-  
+
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   func setTitle() {
     addSubview(titleLabel)
-    
+
     titleLabel.snp.makeConstraints {
       $0.centerX.centerY.equalToSuperview()
     }
+
     titleLabel.text = cvsType.title
     titleLabel.textColor = cvsType.fontColor
-    titleLabel.backgroundColor = cvsType.bgColor
+    titleLabel.backgroundColor = .link
   }
-  
 }
