3e88b749fbd4ee1cf69cf51bdd2ad0fe51f497d2
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-07 13:21:58 +0900

    [ADD] ProductModel

---
+++ PPAK_CVS/Sources/Models/ProductModel.swift
@@ -0,0 +1,29 @@
+//
+//  ProductModel.swift
+//  PPAK_CVS
+//
+//  Created by 홍승현 on 2022/11/06.
+//
+
+import Foundation
+
+struct ProductModel: Codable {
+  /// 이미지 주소
+  let imageLink: String
+  /// 물품명
+  let name: String
+  /// 가격
+  let price: Int
+  /// 편의점 가게
+  let store: String
+  /// 할인종류
+  let saleType: EventType
+
+  enum CodingKeys: String, CodingKey {
+    case name
+    case price
+    case store
+    case imageLink = "img"
+    case saleType = "tag"
+  }
+}
