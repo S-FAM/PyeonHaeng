7c442e2b5c3687d206cc8a0a5fe87b8697de06e4
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-08 16:16:26 +0900

    [ADD] CVSDB's models & constants

--- PPAK_CVS/Sources/Managers/CVSDatabase.swift
+++ PPAK_CVS/Sources/Managers/CVSDatabase.swift
@@ -24,3 +24,29 @@ final class CVSDatabase {
 //  }
 
 }
+
+// MARK: - Models and Constants
+
+extension CVSDatabase {
+
+  enum Error: Swift.Error {
+    
+    /// Firebase를 읽어드리는 데 오류가 발생한 경우
+    case firebase
+    
+    /// 데이터가 잘못 들어와 디코딩이 되지 않는 경우
+    /// 이는 서버측의 데이터타입 문제이거나, 해당 클래스의 타입변경 미스일 때 발생합니다.
+    case decoding
+    
+    /// 서버에 저장되어있는 데이터가 현재 날짜랑 맞지 않는 경우
+    case synchronized
+  }
+
+  private enum Name {
+    static let syncKey = "sync_key"
+  }
+
+  private struct SyncKeyModel: Codable {
+    let month: String
+  }
+}
