7979a7a92beb4e4427f34cdecfd8be4dcb765c65
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-08 22:09:44 +0900

    [CHORE] EventType
    rawValue를 가질 수 있도록 수정
    중복되는 eventTitle property는 삭제

--- PPAK_CVS/Configuration/Constants/Enum/EventType.swift
+++ PPAK_CVS/Configuration/Constants/Enum/EventType.swift
@@ -8,29 +8,8 @@
 import UIKit
 
 /// 이벤트의 종류를 가리키는 Enum Type
-enum EventType: Codable, CaseIterable {
-
-  case onePlusOne
-  case twoPlusOne
-  case allSelectEn
-  case allSelectKr
-
-  var eventTitle: String {
-    switch self {
-    case .onePlusOne: return "1 + 1"
-    case .twoPlusOne: return "2 + 1"
-    case .allSelectEn: return "All"
-    case .allSelectKr: return "전체"
-    }
-  }
+enum EventType: String, Codable, CaseIterable {
+  case onePlusOne = "1+1"
+  case twoPlusOne = "2+1"
+  case all = "All"
 }
-
-
-
-
-///// 이벤트의 종류를 가리키는 Enum Type
-//enum EventType: String, Codable, CaseIterable {
-//  case onePlusOne = "1+1"
-//  case twoPlusOne = "2+1"
-//  case all = "All"
-//}
