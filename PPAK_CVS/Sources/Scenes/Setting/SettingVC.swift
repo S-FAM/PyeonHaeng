f068e505055dd48223eeaebc06b8d48d13abbdd9
isGeekCode <ptuguitar@naver.com>
2022-11-21 21:21:56 +0900

    [ADD] SettingVC init

---
+++ PPAK_CVS/Sources/Scenes/Setting/SettingVC.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+import RxGesture
+
+
+class SettingViewController: BaseViewController, Viewable {
+
+  // MARK: - Properties
+
+  // MARK: - Setup
+  override func setupStyles() {
+    super.setupStyles()
+  }
+
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
