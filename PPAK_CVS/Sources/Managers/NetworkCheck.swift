d3c35543321775011f52b1169857d39937f43575
Minji Kim <minji_0801@naver.com>
2022-11-24 21:23:11 +0900

    [CHORE] code refactoring

--- PPAK_CVS/Sources/Managers/NetworkCheck.swift
+++ PPAK_CVS/Sources/Managers/NetworkCheck.swift
@@ -14,43 +14,48 @@ final class NetworkCheck {
   private let monitor: NWPathMonitor
   public private(set) var isConnected: Bool = false
   public private(set) var topVCIsNetworkVC: Bool = false
-
+  
   // monotior 초기화
   private init() {
     self.monitor = NWPathMonitor()
   }
-
+  
   // Network Monitoring 시작
   public func startMonitoring() {
     self.monitor.start(queue: queue)
     self.monitor.pathUpdateHandler = { [weak self] path in
       guard let self = self else { return }
-
+      
       self.isConnected = path.status == .satisfied
-
-      if self.isConnected {
-        print(#function, "연결됨!")
-
-        if self.topVCIsNetworkVC {
-          DispatchQueue.main.async {
-            guard let topVC = UIApplication.topViewController() else { return }
-            topVC.dismiss(animated: true)
-            self.topVCIsNetworkVC = false
-          }
-        }
-      } else {
-        print(#function, "연결안됨!")
-
-        if self.topVCIsNetworkVC == false {
-          DispatchQueue.main.async {
-            let networkViewController = NetworkViewController()
-            networkViewController.modalPresentationStyle = .overFullScreen
-            guard let topVC = UIApplication.topViewController() else { return }
-            topVC.present(networkViewController, animated: true)
-            self.topVCIsNetworkVC = true
-          }
-        }
+      
+      if self.isConnected && self.topVCIsNetworkVC {
+        // 네트워크와 연결됨, 제일 위에 뷰가 NetworkVC임. -> NetworkVC 사라지기
+        self.dismissNetworkVC()
       }
+      
+      if self.isConnected == false && self.topVCIsNetworkVC == false {
+        // 네트워크와 연결안됨, 제일 위에 뷰가 NetworkVC가 아님. -> NetworkVC 보여주기
+        self.presentNetworkVC()
+      }
+    }
+  }
+  
+  private func presentNetworkVC() {
+    DispatchQueue.main.async {
+      let networkViewController = NetworkViewController()
+      networkViewController.modalPresentationStyle = .overFullScreen
+      
+      guard let topVC = UIApplication.topViewController() else { return }
+      topVC.present(networkViewController, animated: true)
+      self.topVCIsNetworkVC = true
+    }
+  }
+  
+  private func dismissNetworkVC() {
+    DispatchQueue.main.async {
+      guard let topVC = UIApplication.topViewController() else { return }
+      topVC.dismiss(animated: true)
+      self.topVCIsNetworkVC = false
     }
   }
 }
