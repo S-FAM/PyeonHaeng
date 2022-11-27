f3097e03380b6db2cb4a30aa7e407d9906f177f6
eung7 <wannabeeung7@gmail.com>
2022-11-27 22:00:20 +0900

    [Delete] LoadingCell private 제거

--- PPAK_CVS/Sources/Scenes/Common/LoadingCell.swift
+++ PPAK_CVS/Sources/Scenes/Common/LoadingCell.swift
@@ -11,38 +11,40 @@ import SnapKit
 import Then
 
 final class LoadingCell: UICollectionViewCell {
-  
+
   // MARK: - Properties
-  
-  private let indicator = UIActivityIndicatorView().then {
+
+  static let id = "LoadingCell"
+
+  let indicator = UIActivityIndicatorView().then {
     $0.color = .gray
   }
-  
+
   // MARK: - LifeCycle
-  
+
   override init(frame: CGRect) {
     super.init(frame: .zero)
     setupStyles()
     setupLayout()
     setupConstraints()
   }
-  
+
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   // MARK: - Setup
-  
+
   private func setupLayout() {
     contentView.addSubview(indicator)
   }
-  
+
   private func setupConstraints() {
     indicator.snp.makeConstraints { make in
       make.center.equalToSuperview()
     }
   }
-  
+
   private func setupStyles() {
     indicator.startAnimating()
   }
