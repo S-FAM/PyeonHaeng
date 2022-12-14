d52fbc3a15257b66948907d33ba9ec158d02423c
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-14 21:48:51 +0900

    [ADD] default value in TargetType

--- PPAK_CVS/Configuration/Base/TargetType.swift
+++ PPAK_CVS/Configuration/Base/TargetType.swift
@@ -15,3 +15,24 @@ protocol TargetType: URLRequestConvertible {
   var path: String { get }
   var parameters: Parameters { get }
 }
+
+extension TargetType {
+  var baseURL: String {
+    return PrivateKeys.url
+  }
+  
+  func asURLRequest() throws -> URLRequest {
+    let url = try baseURL.asURL()
+    var request = try URLRequest(url: url.appendingPathComponent(path), method: method)
+    
+    // setting headers
+    request.headers = .default
+    
+    // setting uri
+    var components = URLComponents(string: url.appendingPathComponent(path).absoluteString)
+    components?.queryItems = parameters.map { URLQueryItem(name: $0, value: "\($1)") }
+    request.url = components?.url
+    
+    return request
+  }
+}
