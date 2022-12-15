8d3b45f7533c019a4a080f3d1cf1082707def5ca
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-15 15:09:33 +0900

    [ADD] Product search function API

--- PPAK_CVS/Sources/Network/PyeonHaengAPI.swift
+++ PPAK_CVS/Sources/Network/PyeonHaengAPI.swift
@@ -30,4 +30,21 @@ final class PyeonHaengAPI {
     }
     .asObservable()
   }
+
+  func product(named name: String) -> Observable<ResponseModel> {
+    return Single.create { observer in
+      AF.request(ProductTarget.search(name))
+        .responseDecodable(of: ResponseModel.self) { response in
+          switch response.result {
+          case .success(let model):
+            observer(.success(model))
+          case .failure(let error):
+            observer(.failure(error))
+          }
+        }
+
+      return Disposables.create()
+    }
+    .asObservable()
+  }
 }
