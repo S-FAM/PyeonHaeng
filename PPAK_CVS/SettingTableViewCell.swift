24f91346ca6251a776455a01e2327064b40c8d9a
isGeekCode <ptuguitar@naver.com>
2022-12-15 19:58:19 +0900

    [CHORE] SettingTableViewCell - ContainerView를 ContentView에 추가

--- PPAK_CVS/SettingTableViewCell.swift
+++ PPAK_CVS/SettingTableViewCell.swift
@@ -57,7 +57,7 @@ final class SettingTableViewCell: UITableViewCell {
   }
 
   private func setLayouts() {
-    addSubview(containerView)
+    contentView.addSubview(containerView)
     [iconImage, titleLabel, versionInfoLabel].forEach {
       containerView.addSubview($0)
     }
