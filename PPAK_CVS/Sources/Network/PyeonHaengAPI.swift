ffa111b5e3900117c989e57df5a9267ea3cb81af
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-16 21:12:05 +0900

    [REFACTOR] PyeonHaeng APIs
    RequestProductNameModel이 필요없어진 관계로, 관련된 코드 전부 제거

--- PPAK_CVS/Sources/Network/PyeonHaengAPI.swift
+++ PPAK_CVS/Sources/Network/PyeonHaengAPI.swift
@@ -16,24 +16,7 @@ final class PyeonHaengAPI {
 
   func product(typeModel: RequestTypeModel) -> Observable<ResponseModel> {
     return Single.create { observer in
-      AF.request(ProductTarget.filter(typeModel))
-        .responseDecodable(of: ResponseModel.self) { response in
-          switch response.result {
-          case .success(let model):
-            observer(.success(model))
-          case .failure(let error):
-            observer(.failure(error))
-          }
-        }
-
-      return Disposables.create()
-    }
-    .asObservable()
-  }
-
-  func product(nameModel: RequestProductNameModel) -> Observable<ResponseModel> {
-    return Single.create { observer in
-      AF.request(ProductTarget.search(nameModel))
+      AF.request(ProductTarget.search(typeModel))
         .responseDecodable(of: ResponseModel.self) { response in
           switch response.result {
           case .success(let model):
