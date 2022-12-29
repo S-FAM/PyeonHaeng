06d91d96bc1a8408390791312be095e6b31842f7
isGeekCode <ptuguitar@naver.com>
2022-12-29 22:26:30 +0900

    [ADD] SettingCoordinator init

---
+++ PPAK_CVS/Sources/Scenes/Setting/SettingCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  SettingCoordinator.swift
+//  PPAK_CVS
+//
+//  Created by hyeonseok on 2022/12/19.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class SettingCoordinator: BaseCoordinator {
+
+  private let disposeBag = DisposeBag()
+
+  override func start() {
+    let viewModel = SettingViewModel()
+    let viewController = SettingViewController()
+    viewController.coordinator = self
+    viewController.viewModel = viewModel
+    bind(viewModel)
+    self.navigationController.pushViewController(viewController, animated: true)
+  }
+
+  func bind(_ viewModel: SettingViewModel) {}
+}
