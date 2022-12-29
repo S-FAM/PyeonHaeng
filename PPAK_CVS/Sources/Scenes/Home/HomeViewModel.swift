b82d816cf0d31fda389ffc8530b40de00f0f1d59
eung7 <wannabeeung7@gmail.com>
2022-12-29 09:43:57 +0900

    [Fix] HomeViewModel 네이밍 수정 및 축약

--- PPAK_CVS/Sources/Scenes/Home/HomeViewModel.swift
+++ PPAK_CVS/Sources/Scenes/Home/HomeViewModel.swift
@@ -20,13 +20,13 @@ final class HomeViewModel: ViewModel {
     case toggleFilterDropdown
     case toggleShowBookmarkVC(Bool)
     case hideDropdown
-    case updateCVSType(CVSType)
-    case updateSortType(SortType)
-    case updateEventType(EventType)
-    case updateIsLoading(Bool)
-    case updateIsLoadingFromCell(Bool)
-    case updateOffset
-    case updateProducts([ProductModel])
+    case setCVS(CVSType)
+    case setSort(SortType)
+    case setEvent(EventType)
+    case setLoading(Bool)
+    case setLoadingFromCell(Bool)
+    case setOffset
+    case setProducts([ProductModel])
   }
 
   struct State {
@@ -50,10 +50,10 @@ final class HomeViewModel: ViewModel {
       return requestProducts(cvs: .all, event: .all, sort: .none)
 
     case .loadingCellWillDisplay:
-      let nextOffset = currentState.currentOffset + 10
-      return Observable.concat([
-        Observable.just(.updateIsLoadingFromCell(true)),
-        Observable.just(.updateOffset),
+      let nextOffset = currentState.currentOffset + 20
+      return .concat([
+        .just(.setLoadingFromCell(true)),
+        .just(.setOffset),
         requestProducts(
           cvs: currentState.currentCVSType,
           event: currentState.currentEventType,
@@ -62,26 +62,26 @@ final class HomeViewModel: ViewModel {
       ])
 
     case .currentCVSButtonDidTap:
-      return Observable.just(.toggleCVSDropdown)
+      return .just(.toggleCVSDropdown)
 
     case .filterButtonDidTap:
-      return Observable.just(.toggleFilterDropdown)
+      return .just(.toggleFilterDropdown)
 
     case .backgroundDidTap:
-      return Observable.just(.hideDropdown)
+      return .just(.hideDropdown)
 
     case .bookmarkButtonDidTap:
       guard currentState.showBookmarkVC == false else { return .empty() }
-      return Observable.concat([
-        Observable.just(.toggleShowBookmarkVC(true)),
-        Observable.just(.toggleShowBookmarkVC(false))
+      return .concat([
+        .just(.toggleShowBookmarkVC(true)),
+        .just(.toggleShowBookmarkVC(false))
       ])
 
     case .pageControlIndexDidChange(let event):
-      return Observable.concat([
-        Observable.just(.updateIsLoading(true)),
-        Observable.just(.updateEventType(event)),
-        Observable.just(.updateProducts([])),
+      return .concat([
+        .just(.setLoading(true)),
+        .just(.setEvent(event)),
+        .just(.setProducts([])),
         requestProducts(
           cvs: currentState.currentCVSType,
           event: event,
@@ -92,11 +92,11 @@ final class HomeViewModel: ViewModel {
     case .dropdownCVSButtonDidTap(let cvsDropdownCase):
       switch cvsDropdownCase {
       case .cvs(let cvsType):
-        return Observable.concat([
-          Observable.just(.updateIsLoading(true)),
-          Observable.just(.hideDropdown),
-          Observable.just(.updateCVSType(cvsType)),
-          Observable.just(.updateProducts([])),
+        return .concat([
+          .just(.setLoading(true)),
+          .just(.hideDropdown),
+          .just(.setCVS(cvsType)),
+          .just(.setProducts([])),
           requestProducts(
             cvs: cvsType,
             event: currentState.currentEventType,
@@ -106,15 +106,15 @@ final class HomeViewModel: ViewModel {
 
       case .setting:
         // TODO: Setting Page로 이동해야 합니다.
-        return Observable.just(.hideDropdown)
+        return .just(.hideDropdown)
       }
 
     case .dropdownFilterButtonDidTap(let sortType):
-      return Observable.concat([
-        Observable.just(.updateIsLoading(true)),
-        Observable.just(.updateSortType(sortType)),
-        Observable.just(.hideDropdown),
-        Observable.just(.updateProducts([])),
+      return .concat([
+        .just(.setLoading(true)),
+        .just(.setSort(sortType)),
+        .just(.hideDropdown),
+        .just(.setProducts([])),
         requestProducts(cvs: currentState.currentCVSType, event: currentState.currentEventType, sort: sortType)
       ])
     }
@@ -124,14 +124,14 @@ final class HomeViewModel: ViewModel {
     var nextState = state
 
     switch mutation {
-    case .updateProducts(let products):
+    case .setProducts(let products):
       if products.isEmpty {
         nextState.products = []
       } else {
         nextState.products += products
       }
 
-    case .updateIsLoading(let isLoading):
+    case .setLoading(let isLoading):
       nextState.isLoading = isLoading
 
     case .toggleCVSDropdown:
@@ -147,21 +147,21 @@ final class HomeViewModel: ViewModel {
     case let .toggleShowBookmarkVC(isShowBookmarkVC):
       nextState.showBookmarkVC = isShowBookmarkVC
 
-    case .updateIsLoadingFromCell(let isLoading):
+    case .setLoadingFromCell(let isLoading):
       nextState.isLoadingFromCell = isLoading
 
-    case .updateOffset:
-      nextState.currentOffset += 10
+    case .setOffset:
+      nextState.currentOffset += 20
 
-    case .updateEventType(let eventType):
+    case .setEvent(let eventType):
       nextState.currentEventType = eventType
       nextState.currentOffset = 0
 
-    case .updateCVSType(let cvsType):
+    case .setCVS(let cvsType):
       nextState.currentCVSType = cvsType
       nextState.currentOffset = 0
 
-    case .updateSortType(let sortType):
+    case .setSort(let sortType):
       nextState.currentSortType = sortType
       nextState.currentOffset = 0
 
@@ -175,19 +175,20 @@ extension HomeViewModel {
     cvs: CVSType,
     event: EventType,
     sort: SortType,
-    offset: Int = 0
+    offset: Int = 0,
+    name: String = ""
   ) -> Observable<Mutation> {
     return Observable.concat([
-      CVSDatabase.shared.product(
-        request: RequestTypeModel(
+      PyeonHaengAPI.shared.product(request: RequestTypeModel(
         cvs: cvs,
         event: event,
-        sort: sort),
+        sort: sort,
+        name: name,
         offset: offset
-      )
-      .flatMap { Observable.just(.updateProducts($0)) },
-      Observable.just(.updateIsLoading(false)),
-      Observable.just(.updateIsLoadingFromCell(false))
+      ))
+      .flatMap { Observable.just(.setProducts($0.products)) },
+      .just(.setLoading(false)),
+      .just(.setLoadingFromCell(false))
     ])
   }
 }
