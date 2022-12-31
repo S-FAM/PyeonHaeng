993399f6ea972a28f0be64420738034e2ff96d29
Minji Kim <minji_0801@naver.com>
2022-12-31 15:37:08 +0900

    [ADD] Custom Fonts

---
+++ PPAK_CVS/Configuration/Extensions/UIFont+.swift
@@ -0,0 +1,32 @@
+//
+//  UIFont+.swift
+//  PPAK_CVS
+//
+//  Created by 김민지 on 2022/12/31.
+//
+
+import UIKit
+
+extension UIFont {
+
+  enum Family: String {
+    case black = "Black"
+    case bold = "Bold"
+    case extraBold = "ExtraBold"
+    case extraLight = "ExtraLight"
+    case light = "Light"
+    case medium = "Medium"
+    case regular = "Regular"
+    case semiBold = "SemiBold"
+    case thin = "Thin"
+  }
+
+  /// ```
+  /// (For example)
+  /// UIFont.appFont(family: .regular, size: 15.0)
+  /// ```
+  static func appFont(family: Family, size: CGFloat) -> UIFont {
+    let appFont = UIFont(name: "Pretendard-\(family.rawValue)", size: size)
+    return appFont ?? UIFont.systemFont(ofSize: size)
+  }
+}
