d9759c386383ba86681317e27d6f5d8b743eba48
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-02 19:11:08 +0900

    [ADD] Properties in ProductCoordinator
    ProductCoordinator가 ProductModel을 갖기 때문에 initialization을 추가로 구현했습니다.
    기존의 initialization은 model을 초기화하지 못하므로 private로 접근제어를 설정했습니다.

--- PPAK_CVS/Sources/Scenes/Product/ProductCoordinator.swift
+++ PPAK_CVS/Sources/Scenes/Product/ProductCoordinator.swift
@@ -9,6 +9,25 @@ import UIKit
 
 final class ProductCoordinator: BaseCoordinator {
 
+  // MARK: - Properties
+
+  private let model: ProductModel
+  private let disposeBag = DisposeBag()
+
+  // MARK: - Initializations
+
+  init(_ navigationController: UINavigationController, model: ProductModel) {
+    self.model = model
+    super.init(navigationController: navigationController)
+  }
+
+  private override init(navigationController: UINavigationController) {
+    self.model = ProductModel(imageLink: "", name: "", price: 0, store: .all, saleType: .all)
+    super.init(navigationController: navigationController)
+  }
+
+  // MARK: - Coordinator functions
+
   override func start() {
     let viewController = ProductViewController()
     let viewModel = ProductViewModel()
