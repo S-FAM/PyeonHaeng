95b6f9a85851f8916d10405a47ad22c1fbcff0ef
isGeekCode <ptuguitar@naver.com>
2023-01-19 21:35:29 +0900

    [CHORE] SettingVC - 메일전송실패 메시지 수정

--- PPAK_CVS/Sources/Scenes/Setting/SettingConfig.swift
+++ PPAK_CVS/Sources/Scenes/Setting/SettingConfig.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Message {
 
   static let sendMailFail = "메일을 보낼 수 없습니다."
-  static let retry = "계정을 확인하고 다시 시도해주세요."
+  static let retry = "메일을 보내려면 'Mail' 앱이 필요합니다.\nApp Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요."
   static let email = "bang.hyeonseok.dev@gmail.com"
 
   static let cancel = "취소"
