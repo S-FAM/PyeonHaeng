2e5e1a37e4622ab1f3cb1edba8c02f479386a0c7
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-17 18:20:59 +0900

    [FEAT] Implement network for previous event history

--- PPAK_CVS/Sources/Network/PyeonHaengAPI.swift
+++ PPAK_CVS/Sources/Network/PyeonHaengAPI.swift
@@ -30,4 +30,21 @@ final class PyeonHaengAPI {
     }
     .asObservable()
   }
+
+  func history(request: RequestTypeModel) -> Observable<ResponseModel> {
+    return Single.create { observer in
+      AF.request(ProductTarget.history(request))
+        .responseDecodable(of: ResponseModel.self) { response in
+          switch response.result {
+          case .success(let model):
+            observer(.success(model))
+          case .failure(let error):
+            observer(.failure(error))
+          }
+        }
+
+      return Disposables.create()
+    }
+    .asObservable()
+  }
 }
