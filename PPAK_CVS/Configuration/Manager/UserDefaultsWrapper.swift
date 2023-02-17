c71316af8ed7f248ada87d30d4b2835782470a76
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-17 14:42:19 +0900

    [FEAT] UserDefaultsWrapper
    UserDefaults를 이용할 때 편리하게 사용하기 위해 구현하였습니다.

--- PPAK_CVS/Configuration/Manager/UserDefaultsWrapper.swift
+++ PPAK_CVS/Configuration/Manager/UserDefaultsWrapper.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct UserDefaultsWrapper<T: Codable> {
+
+  private let key: String
+  private let defaultValue: T
+
+  init(key: String, defaultValue: T) {
+    self.key = key
+    self.defaultValue = defaultValue
+  }
+
+  var wrappedValue: T {
+    get {
+      guard let data = UserDefaults.standard.object(forKey: self.key) as? Data,
+            let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+        return defaultValue
+      }
+      return decodedData
+    }
+    set {
+      guard let data = try? JSONEncoder().encode(newValue)
+      else {
+        UserDefaults.standard.removeObject(forKey: key)
+        return
+      }
+      UserDefaults.standard.set(data, forKey: key)
+    }
+  }
+}
