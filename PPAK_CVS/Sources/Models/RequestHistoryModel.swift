5d2e7f646d3dea13221e7bfd41bf4088ef127267
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-21 20:19:52 +0900

    [ADD] RequestHistoryModel
    기존 RequestTypeModel의 cvs와 name값만을 가지는 모델입니다.
    서버에서는 두 옵션만을 요청으로 받지만, RequestTypeModel는 불필요한 프로퍼티도 갖고 있습니다.
    따라서 요청 모델을 둘로 분리하여 각 역할에 맞게 구성하는 것이 옳다고 판단하였고
    기존의 모델명 컨벤션에 맞게 `RequestHistoryModel`를 추가하였습니다.

---
+++ PPAK_CVS/Sources/Models/RequestHistoryModel.swift
@@ -0,0 +1,13 @@
+//
+//  RequestHistoryModel.swift
+//  PPAK_CVS
+//
+//  Created by 홍승현 on 2023/02/21.
+//
+
+import Foundation
+
+struct RequestHistoryModel: Codable {
+  let cvs: CVSType
+  let name: String
+}
