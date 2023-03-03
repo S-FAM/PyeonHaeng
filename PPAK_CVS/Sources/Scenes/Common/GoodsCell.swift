7a5b0687a845d07aae9ece8d1277082d742a6b0c
eung7 <wannabeeung@gmail.com>
2023-03-03 14:12:00 +0900

    [Feat] GoodsCell isSkeletonable 적용 및 Mock데이터 적용

--- PPAK_CVS/Sources/Scenes/Common/GoodsCell.swift
+++ PPAK_CVS/Sources/Scenes/Common/GoodsCell.swift
@@ -22,14 +22,19 @@ final class GoodsCell: UICollectionViewCell {
     $0.layer.shadowOpacity = 0.1
     $0.layer.shadowColor = UIColor.black.cgColor
     $0.backgroundColor = .white
+    $0.isSkeletonable = true
   }
 
   private let goodsLabel = UILabel().then {
+    $0.text = "삼양사)상쾌한스틱18g"
     $0.font = .appFont(family: .bold, size: 14)
+    $0.isSkeletonable = true
   }
 
   private let priceLabel = UILabel().then {
+    $0.text = "3900원(개당 1,450원)"
     $0.font = .appFont(family: .regular, size: 14)
+    $0.isSkeletonable = true
   }
 
   private let goodsImage = UIImageView().then {
@@ -41,13 +46,19 @@ final class GoodsCell: UICollectionViewCell {
     [goodsLabel, priceLabel, saleTypeView].forEach {
       stackView.addArrangedSubview($0)
     }
-      stackView.axis = .vertical
-      stackView.spacing = 6
-      stackView.alignment = .leading
+    stackView.axis = .vertical
+    stackView.spacing = 6
+    stackView.alignment = .leading
+    stackView.isSkeletonable = true
   }
 
-  private let titleLogoView = TitleLogoView(cvsType: .all)
-  private let saleTypeView = SaleTypeView(cvsType: .all)
+  private let titleLogoView = TitleLogoView(cvsType: .all).then {
+    $0.isSkeletonable = true
+  }
+
+  private let saleTypeView = SaleTypeView(cvsType: .all).then {
+    $0.isSkeletonable = true
+  }
 
   // MARK: - LifeCycle
 
