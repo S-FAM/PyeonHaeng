bd5ad88d6bd16ec691069781e04ff50d231a8605
Minji Kim <kimminji080122@gmail.com>
2023-03-08 23:25:48 +0900

    [CHORE] change 7eleven badge icon

--- PPAK_CVS/Configuration/Constants/Enum/CvsType.swift
+++ PPAK_CVS/Configuration/Constants/Enum/CvsType.swift
@@ -59,7 +59,7 @@ enum CVSType: String, Codable {
     switch self {
     case .cu: return UIImage(named: "badge_cu")
     case .gs: return UIImage(named: "badge_gs25")
-    case .sevenEleven: return UIImage(named: "badge_cu")
+    case .sevenEleven: return UIImage(named: "badge_7eleven")
     case .miniStop: return UIImage(named: "badge_cu")
     case .eMart: return UIImage(named: "badge_emart24")
     case .all: return UIImage(named: "badge_cu")
