e841e4c0931e8f624d72b6af86219e1bce404820
Minji Kim <kimminji080122@gmail.com>
2023-03-08 23:14:12 +0900

    [CHORE] change emart24 badge icon

--- PPAK_CVS/Configuration/Constants/Enum/CvsType.swift
+++ PPAK_CVS/Configuration/Constants/Enum/CvsType.swift
@@ -61,7 +61,7 @@ enum CVSType: String, Codable {
     case .gs: return UIImage(named: "badge_gs25")
     case .sevenEleven: return UIImage(named: "badge_cu")
     case .miniStop: return UIImage(named: "badge_cu")
-    case .eMart: return UIImage(named: "badge_cu")
+    case .eMart: return UIImage(named: "badge_emart24")
     case .all: return UIImage(named: "badge_cu")
     }
   }
